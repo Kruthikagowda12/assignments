004abef62c4c414d02b916d860521505014931b0
Kruthikagowda12 <kruthikakj2002@gmail.com>
2026-01-21 10:54:42 +0530

    program1

---
+++ python assignmets/pg1.py
@@ -0,0 +1,8 @@
+# 1. Write a Python program to calculate the length of a string.
+s=input("Enter a string: ")
+def  func(s):
+    count=0
+    for i in s:
+        count+=1
+    return count
+print(func(s))
